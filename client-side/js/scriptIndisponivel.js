4cc277c987298bce92765ae98b65b43634c30d1f
GustavoCodou <gustavoguimaraescode@gmail.com>
2024-11-25 19:22:49 -0300

    Pagina de start
    FIz a pagina de start do sistema que o mano Mateus pediu.

---
+++ client-side/js/scriptIndisponivel.js
@@ -0,0 +1,7 @@
+// Seleciona os botões
+const buttonvoltar = document.getElementById("voltar");
+
+// Adiciona eventos aos botões
+buttonvoltar.addEventListener("click", function() {
+    window.location.href = 'startpage.html'; 
+});
